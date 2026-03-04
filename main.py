e649ffa56d060d0e5ae5dbd7fe2bdef5eec584f4
Arian Nasr <arian@2ari.ca>
2026-03-04 02:22:31 -0500

    multi-file uploads

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, request, redirect
+from flask import Flask, request
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = '/opt/navidrome/music'
@@ -15,31 +15,31 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # File saved successfully, return a success message
-            return f'''
-            <h1>File {filename} uploaded successfully</h1>
-            <a href="{request.url}"><button>Upload another file</button></a>
-            ''', 200
+            return 'No file part', 400
+        files = request.files.getlist('file')
+        for file in files:
+            if file.filename == '':
+                return 'No selected file', 400
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            else:
+                return 'File extension not allowed', 400
+        return f'''
+        <!doctype html>
+        <title>Upload successful</title>
+        <h1>{len(files)} file(s) uploaded successfully</h1>
+        <a href="{request.url}"><button>Upload another file</button></a>
+        ''', 200
+
     return '''
     <!doctype html>
     <title>Upload new File</title>
     <button onclick="window.location.href='/outpost.goauthentik.io/sign_out'">Logout</button>
     <h1>Upload new File</h1>
     <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
+      <input type=file name=file multiple>
       <input type=submit value=Upload>
     </form>
     '''
