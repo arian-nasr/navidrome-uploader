9dc6aa8d04f2a1e6d1b9cd1b5f3f88478bdab0ba
Arian Nasr <arian@2ari.ca>
2026-04-08 23:29:05 -0400

    tidying

--- release/preinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if ! getent passwd navidrome-uploader > /dev/null 2>&1; then
-    printf "Creating navidrome-uploader user\n"
-    useradd --system --shell /usr/sbin/nologin --user-group navidrome-uploader
-fi
