6e22dcdda501b6aa3f2b3d0bcacce80b201d616d
Arian Nasr <arian@2ari.ca>
2026-06-01 03:34:41 -0400

    fix(tests): change edge case file extension
    Signed-off-by: Arian Nasr <arian@2ari.ca>

--- e2e/unit/api/test_api.py
+++ e2e/unit/api/test_api.py
@@ -54,9 +54,9 @@ def test_api_upload_flac_file(base_url, upload_folder):
     if os.path.exists(expected_filename):
         os.remove(expected_filename)
 
-def test_api_upload_wav_file(base_url, upload_folder):
-    files = {'file': ('test.wav', io.BytesIO(b'fake wav content'))}
-    expected_filename = os.path.join(upload_folder, secure_filename('test.wav'))
+def test_api_upload_m4a_file(base_url, upload_folder):
+    files = {'file': ('test.m4a', io.BytesIO(b'fake m4a content'))}
+    expected_filename = os.path.join(upload_folder, secure_filename('test.m4a'))
     response = requests.post(f'{base_url}/', files=files)
     assert response.status_code == 400
     assert "not allowed" in response.json().get("error", "")
